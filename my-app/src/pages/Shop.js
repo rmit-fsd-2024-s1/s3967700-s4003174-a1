61e6929e3f5ae7f33b3f5fb0d25cb42576dc880e
Ninh Huynh <s4003174@student.rmit.edu.au>
2024-04-08 00:00:19 +1000

    push

--- my-app/src/pages/Shop.js
+++ my-app/src/pages/Shop.js
@@ -1,56 +1,77 @@
 import React from "react";
 import food from './organic.jpg';
 import "./page.css";
-import { Link } from "react-router-dom";
 
 function Home(props) {
   return (
     <>
       <div className="main">
         <div className="text-center">
-          <h2>Check out our special deals</h2>
+          <h2>All ingrediants are grown locally and organic</h2>
         </div>
-        <br />
-        <div className="row">
-          <div className="text-options-1">
-          <h3>Shop Organic Groceries</h3>
-          </div>
-          <div className="text-options-2">
-            <h3>Weekly Special Deals</h3>
-          </div>
-          <div className="text-options-3">
-            <h3>Grow your own foods</h3>
-          </div>
-        </div>
+        <br/>
+        <br/>
+        <br/>
         <div className="row">
           <div className="col-lg-4">
-          <Link className="nav-link" to="/shop">
           <img src={food} alt="Organic Food" style={{ height: 200 }} />
-          </Link>
+          <h4 className="caption">Advocado</h4>
           </div>
           <div className="col-lg-4">
-          <Link className="nav-link" to="/profile">
           <img src={food} alt="Organic Food" style={{ height: 200 }} />
-          </Link>
+          <h4 className="caption">Beetroot</h4>
           </div>
           <div className="col-lg-4">
-          <Link className="nav-link" to="/profile">
           <img src={food} alt="Organic Food" style={{ height: 200 }} />
-          </Link>
+          <h4 className="caption">Carrot</h4>
           </div>
         </div>
-        <br />
+        <br/>
         <div className="row">
-      <div>
-        <p className='para-options-1'>Shop now for the freshest foods</p>
-      </div>
-      <div>
-        <p className="para-options-2">Check out our weekly specials</p>
-      </div>
-      <div>
-        <p className="para-options-3">Grow your own organic food</p>
-      </div>
-    </div>
+          <div className="col-lg-4">
+          <img src={food} alt="Organic Food" style={{ height: 200 }} />
+          <h4 className="caption">Chilli</h4>
+          </div>
+          <div className="col-lg-4">
+          <img src={food} alt="Organic Food" style={{ height: 200 }} />
+          <h4 className="caption">Daikon</h4>
+          </div>
+          <div className="col-lg-4">
+          <img src={food} alt="Organic Food" style={{ height: 200 }} />
+          <h4 className="caption">Eggplant</h4>
+          </div>
+        </div>
+        <br/>
+        <div className="row">
+          <div className="col-lg-4">
+          <img src={food} alt="Organic Food" style={{ height: 200 }} />
+          <h4 className="caption">Fennel</h4>
+          </div>
+          <div className="col-lg-4">
+          <img src={food} alt="Organic Food" style={{ height: 200 }} />
+          <h4 className="caption">Garlic</h4>
+          </div>
+          <div className="col-lg-4">
+          <img src={food} alt="Organic Food" style={{ height: 200 }} />
+          <h4 className="caption">Kale</h4>
+          </div>
+        </div>
+        <br/>
+        <div className="row">
+          <div className="col-lg-4">
+          <img src={food} alt="Organic Food" style={{ height: 200 }} />
+          <h4 className="caption">Lettuce</h4>
+          </div>
+          <div className="col-lg-4">
+          <img src={food} alt="Organic Food" style={{ height: 200 }} />
+          <h4 className="caption">Mushroom</h4>
+          </div>
+          <div className="col-lg-4">
+          <img src={food} alt="Organic Food" style={{ height: 200 }} />
+          <h4 className="caption">Tomato</h4>
+          </div>
+        </div>
+        <br/>
       </div>
     </>
   );
