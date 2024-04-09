cd3dfba7bcd45af025893a88966de4e500db5678
Ninh Huynh <s4003174@student.rmit.edu.au>
2024-04-09 16:05:19 +1000

    pushing food

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -1,69 +1,77 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import food from './images/organic.jpg';
 import special from './images/specials.jpg';
 import "./page.css";
-import { Link } from "react-router-dom";
 
-function Home(props) {
+function Section({ title, imageSrc, buttonText, buttonLink, caption }) {
   return (
-    <>
-      <div className="main">
-        <div className="text-center">
-          <h2>Food delivered to you 24/7</h2>
-          <img src={food} alt="Organic Food" style={{ height: 300 }}/>
-        </div>
-        <br />
-        <p className='content'>
-          Here at SOIL we offer 24/7 fast and fresh organic foods. 
-          Our organisation aims to provide our customers with local foods 
-          provided by our local farmers. Every purchase made helps us get one 
-          step closer to a greener and healthier planet.
-        </p>
-        <br />
-        <div className="row">
-          <div className="text-options-1">
+    <div className="col-lg-4">
+      <Link className="nav-link" to={buttonLink}>
+        <img src={imageSrc} alt={title} style={{ height: 200 }} />
+      </Link>
+      <Link className="shop-button" to={buttonLink}>
+        <button>{buttonText}</button>
+      </Link>
+      <p className="caption">{caption}</p>
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div className="main">
+      <div className="text-center">
+        <h2>Food delivered to you 24/7</h2>
+        <img src={food} alt="Organic Food" style={{ height: 300 }} />
+      </div>
+      <br />
+      <p className='content'>
+        Here at SOIL we offer 24/7 fast and fresh organic foods. 
+        Our organisation aims to provide our customers with local foods 
+        provided by our local farmers. Every purchase made helps us get one 
+        step closer to a greener and healthier planet.
+      </p>
+      <br />
+      <div className="row">
+        <div className="text-options-1">
           <h3>Shop Organic Groceries</h3>
-          </div>
-          <div className="text-options-2">
-            <h3>Weekly Special Deals</h3>
-          </div>
-          <div className="text-options-3">
-            <h3>Grow your own foods</h3>
-          </div>
         </div>
-        <div className="row">
-          <div className="col-lg-4">
-          <Link className="nav-link" to="/shop">
-          <img src={food} alt="Organic Food" style={{ height: 200 }} />
-          </Link>
-          <Link className="shop-button" to="/shop">
-          <button>Shop Now</button>
-          </Link>
-          <p className='caption'>Shop now for the freshest foods</p>
-          </div>
-          <div className="col-lg-4">
+        <div className="text-options-2">
+          <h3>Weekly Special Deals</h3>
+        </div>
+        <div className="text-options-3">
+          <h3>Grow your own foods</h3>
+        </div>
+      </div>
+      <div className="row">
+        <Section
+          title="Organic Food"
+          imageSrc={food}
+          buttonText="Shop Now"
+          buttonLink="/shop"
+          caption="Shop now for the freshest foods"
+        />
+        <Section
+          title="Special Deals"
+          imageSrc={special}
+          buttonText="Shop Now"
+          buttonLink="/specials"
+          caption="Check out our weekly specials"
+        />
+        <div className="col-lg-4">
           <Link className="nav-link" to="/profile">
-          <img src={special} alt="Special Deals" style={{ height: 200 }} />
-          </Link>
-          <Link className="shop-button" to="/shop">
-          <button>Shop Now</button>
-          </Link>
-          <p className="caption">Check out our weekly specials</p>
-          </div>
-          <div className="col-lg-4">
-          <Link className="nav-link" to="/profile">
-          <img src={food} alt="Organic Food" style={{ height: 200 }} />
+            <img src={food} alt="Organic Food" style={{ height: 200 }} />
           </Link>
           <p className="caption">Grow your own organic food</p>
-          </div>
         </div>
-        <br />
       </div>
-    </>
+    </div>
   );
 }
 
 export default Home;
+
 
 
 
--- my-app/src/pages/Shop.js
+++ my-app/src/pages/Shop.js
@@ -1,79 +1,62 @@
 import React from "react";
-import food from './images/organic.jpg';
+import Advocado from './images/Advocado.jpg';
+import Cabbage from './images/Cabbage.jpg';
+import Carrot from './images/Carrot.jpg';
+import Chillies from './images/Chillies.jpg';
+import Cucumber from './images/Cucumber.jpg';
+import Eggplant from './images/Eggplant.jpg';
+import Garlic from './images/Garlic.jpg';
+import Kale from './images/Kale.jpg';
+import Lettuce from './images/Lettuce.jpg';
+import Mushrooms from './images/Mushrooms.jpg';
+import Onions from './images/Onions.jpg';
+import Tomatoes from './images/Tomatoes.jpg';
 import "./page.css";
 
-function Shop(props) {
-  return (
-    <>
-      <div className="main">
-        <div className="text-center">
-          <h2>All ingrediants are grown locally and organic</h2>
-        </div>
-        <br/>
-        <br/>
-        <br/>
-        <div className="row">
-          <div className="col-lg-4">
-          <img src={food} alt="Organic Food" style={{ height: 200 }} />
-          <h4 className="caption-shop">Advocado - $5.35</h4>
-          </div>
-          <div className="col-lg-4">
-          <img src={food} alt="Organic Food" style={{ height: 200 }} />
-          <h4 className="caption-shop">Cabbage - $8.20</h4>
-          </div>
-          <div className="col-lg-4">
-          <img src={food} alt="Organic Food" style={{ height: 200 }} />
-          <h4 className="caption-shop">Carrot - $4.55</h4>
-          </div>
-        </div>
-        <br/>
-        <div className="row">
-          <div className="col-lg-4">
-          <img src={food} alt="Organic Food" style={{ height: 200 }} />
-          <h4 className="caption-shop">Chillies 100g - $5.29</h4>
-          </div>
-          <div className="col-lg-4">
-          <img src={food} alt="Organic Food" style={{ height: 200 }} />
-          <h4 className="caption-shop">Cucumber - $5.99</h4>
-          </div>
-          <div className="col-lg-4">
-          <img src={food} alt="Organic Food" style={{ height: 200 }} />
-          <h4 className="caption-shop">Eggplant - $5.95</h4>
-          </div>
-        </div>
-        <br/>
-        <div className="row">
-          <div className="col-lg-4">
-          <img src={food} alt="Organic Food" style={{ height: 200 }} />
-          <h4 className="caption-shop">Fennel - $4.97</h4>
-          </div>
-          <div className="col-lg-4">
-          <img src={food} alt="Organic Food" style={{ height: 200 }} />
-          <h4 className="caption-shop">Garlic 100g - $8.92</h4>
-          </div>
-          <div className="col-lg-4">
-          <img src={food} alt="Organic Food" style={{ height: 200 }} />
-          <h4 className="caption-shop">Kale - $5.47</h4>
-          </div>
-        </div>
-        <br/>
-        <div className="row">
-          <div className="col-lg-4">
-          <img src={food} alt="Organic Food" style={{ height: 200 }} />
-          <h4 className="caption-shop">Lettuce - $5.99</h4>
-          </div>
-          <div className="col-lg-4">
-          <img src={food} alt="Organic Food" style={{ height: 200 }} />
-          <h4 className="caption-shop">Mushrooms 150g - $7.79</h4>
-          </div>
-          <div className="col-lg-4">
-          <img src={food} alt="Organic Food" style={{ height: 200 }} />
-          <h4 className="caption-shop">Tomatoes 500g - $9.79</h4>
-          </div>
-        </div>
-        <br/>
+function Shop() {
+  const items = [
+    { itemName: 'Advocado', itemLink: Advocado, price: 5.35 },
+    { itemName: 'Cabbage', itemLink: Cabbage, price: 8.21 },
+    { itemName: 'Carrots', itemLink: Carrot, price: 4.55 },
+    { itemName: 'Chillies 100g', itemLink: Chillies, price: 5.29 },
+    { itemName: 'Cucumber', itemLink: Cucumber, price: 5.99 },
+    { itemName: 'Eggplant', itemLink: Eggplant, price: 5.95 },
+    { itemName: 'Garlic 100g', itemLink: Garlic, price: 8.92 },
+    { itemName: 'Kale', itemLink: Kale, price: 5.47 },
+    { itemName: 'Lettuce', itemLink: Lettuce, price: 5.99 },
+    { itemName: 'Mushrooms 150g', itemLink: Mushrooms, price: 7.79 },
+    { itemName: 'Onions', itemLink: Onions, price: 4.97 },
+    { itemName: 'Tomatoes 500g', itemLink: Tomatoes, price: 9.79 },
+  ];
+
+  function ShopItem({ itemName, itemLink, price }) {
+    return (
+      <div className="col-lg-4">
+        <img src={itemLink} alt={itemName} style={{ height: 200 }} />
+        <h4 className="caption-shop">{itemName} - ${price}</h4>
       </div>
-    </>
+    );
+  }
+
+  return (
+    <div className="main">
+      <div className="text-center">
+        <h2>All ingredients are grown locally and organic</h2>
+      </div>
+      <br />
+      <br />
+      <br />
+      <div className="row">
+        {items.map((item, index) => (
+          <ShopItem
+            key={index}
+            itemName={item.itemName}
+            itemLink={item.itemLink}
+            price={item.price}
+          />
+        ))}
+      </div>
+    </div>
   );
 }
 
