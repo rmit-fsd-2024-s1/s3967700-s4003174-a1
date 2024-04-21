e9e1f6becc93dd06f9464faf70a302761bcf5012
Bilal3967700 <s3967700@student.rmit.edu.au>
2024-04-21 13:09:02 +1000

    updated footer

--- my-app/src/fragments/Footer.js
+++ my-app/src/fragments/Footer.js
@@ -1,11 +1,11 @@
 import React from "react";
+import "./Fragment.css";
 
 function Footer() {
   return (
     <footer className="footer bg-dark text-white py-4">
       <div className="container">
         <div className="row">
-
           <div className="col-md-3">
             <h5>Contact Us</h5>
             <ul className="list-unstyled">
@@ -13,18 +13,14 @@ function Footer() {
               <li>info@soil.com</li>
             </ul>
           </div>
-
-
           <div className="col-md-3">
             <h5>Quick Links</h5>
             <ul className="list-unstyled">
-              <li><a href="/about" className="text-light">About Us</a></li>
-              <li><a href="/faq" className="text-light">FAQs</a></li>
-              <li><a href="/privacy" className="text-light">Privacy Policy</a></li>
+              <li><a className="text-light">About Us</a></li>
+              <li><a className="text-light">FAQs</a></li>
+              <li><a className="text-light">Privacy Policy</a></li>
             </ul>
           </div>
-
-
           <div className="col-md-3">
             <h5>Follow Us</h5>
             <ul className="list-unstyled">
@@ -33,14 +29,11 @@ function Footer() {
               <li><a href="https://twitter.com" className="text-light">Twitter</a></li>
             </ul>
           </div>
-
-
           <div className="col-md-3">
             <h5>Sustainability</h5>
             <p>We are committed to reducing our carbon footprint and supporting local farmers by providing organic and sustainable products.</p>
           </div>
         </div>
-
         <div className="text-center mt-3">
           Healthy, Fresh, Organic SOIL &copy; 2024
         </div>
